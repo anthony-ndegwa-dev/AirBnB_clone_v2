bf2efef4bf8cd432f3b140299ccd98d8faaaf6a7
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-22 01:42:46 -0700

    Define table name cities

--- models/city.py
+++ models/city.py
@@ -8,8 +8,9 @@ from sqlalchemy.orm import relationship
 from models.place import Place
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
