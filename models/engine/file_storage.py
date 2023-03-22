9e8434d76353b2cfcb5aad52ea70765bbf500251
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-22 07:36:41 -0700

    Modify methods and attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,12 +12,20 @@ import shlex
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
         dic = {}
         if cls:
             dictionary = self.__objects
@@ -31,13 +39,17 @@ class FileStorage:
             return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """serialize the file path to JSON file path
+        """
         my_dict = {}
         for key, value in self.__objects.items():
             my_dict[key] = value.to_dict()
@@ -45,7 +57,8 @@ class FileStorage:
             json.dump(my_dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """serialize the file path to JSON file path
+        """
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
                 for key, value in (json.load(f)).items():
@@ -53,9 +66,9 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
-        
+
     def delete(self, obj=None):
-        """ delete existing objects
+        """ delete an existing element
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
