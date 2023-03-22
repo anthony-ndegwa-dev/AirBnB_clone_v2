db8a470c9019126382e993ec3813036586882f57
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-22 00:37:33 -0700

    Define table name amenities

--- models/amenity.py
+++ models/amenity.py
@@ -11,5 +11,6 @@ class Amenity(BaseModel):
     Attributes:
         name: input name
     """
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
