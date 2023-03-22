0c1810a083d12ea90a59ab3aab6f440e222993be
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-22 02:58:23 -0700

    Add Base attribute

--- models/review.py
+++ models/review.py
@@ -5,9 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
