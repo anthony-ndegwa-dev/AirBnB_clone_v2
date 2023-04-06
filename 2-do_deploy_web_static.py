c6cc486b716bf48b32b5b3c2f57d1c957c8a59b4
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-04-06 12:12:43 -0700

    Fix pycodestyle errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -42,23 +42,26 @@ def do_deploy(archive_path):
         archive_filename = os.path.basename(archive_path)
         archive_name = os.path.splitext(archive_filename)[0]
         run("sudo mkdir -p /data/web_static/releases/{}/"
-                .format(archive_name))
+            .format(archive_name))
         run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-                .format(archive_filename, archive_name))
+            .format(archive_filename, archive_name))
 
         # Delete the archive from the web server
         run("sudo rm /tmp/{}".format(archive_filename))
 
         # Move files from archive to the right path
-        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(archive_name, archive_name))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+           /data/web_static/releases/{}/".format(archive_name, archive_name))
+
         run("sudo rm -rf /data/web_static/releases/{}/web_static"
-                .format(archive_name))
+            .format(archive_name))
 
         # Delete symbolic link /data/web_static/current from the web server
         run("sudo rm -rf /data/web_static/current")
 
         # Create new symbolic link /data/web_static/current on the web server
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_name))
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive_name))
 
         print("New version deployed!")
         return True
