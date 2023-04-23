b0915262da095355a2d49942a3c78e7c3b4ad3fa
root <anthony.n.ndegwa@gmail.com>
2023-04-23 03:55:42 -0700

    Fix pycodestyle errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -20,8 +20,8 @@ IMPORTANT
 
     Make sure you have a running and valid setup_mysql_dev.sql in your
      AirBnB_clone_v2 repository (Task)
-    Make sure all tables are created when you run echo "quit" | 
-     HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd 
+    Make sure all tables are created when you run echo "quit" |
+     HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
      HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db
      HBNB_TYPE_STORAGE=db ./console.py
 """
@@ -47,6 +47,7 @@ def display_html():
     return render_template('7-states_list.html',
                            Table="States",
                            items=dict_to_html)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
